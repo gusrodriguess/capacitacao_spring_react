0bc2a726ed2ec311278800df13b04a1970a185b0
gusrodriguess <gustavorodrigues_170@outlook.com>
2026-06-08 12:17:01 -0300

    fix: ajuste na configuracao do cors

--- backend/src/main/java/com/include/capacitacao/controller/ConvidadoController.java
+++ backend/src/main/java/com/include/capacitacao/controller/ConvidadoController.java
@@ -3,6 +3,7 @@ package com.include.capacitacao.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.include.capacitacao.service.ConvidadoService;
 
 @RequestMapping("/convidados")
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ConvidadoController {
 
     @Autowired
